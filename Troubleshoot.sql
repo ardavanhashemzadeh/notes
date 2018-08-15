cff4cb59472c6e21ed781ce46017d17861e266d4
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2018-08-14 20:13:11 -0400

    Create Troubleshoot.sql

---
+++ Troubleshoot.sql
@@ -0,0 +1,10 @@
+SHOW variables WHERE
+variable_name LIKE "%port"
+OR variable_name LIKE "server-id"
+OR variable_name LIKE "log-bin"
+OR variable_name LIKE "innodb_flush_log_at_trx_commit"
+OR variable_name LIKE "sync_binlog"
+OR variable_name LIKE "bind_address"
+OR variable_name LIKE "max_allowed_packet"
+OR variable_name LIKE "wait_timeout"
+OR variable_name LIKE "interactive_timeout"
